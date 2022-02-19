75fc3ab76aea3ddab54ede73fe94508dc84d97a9
Mosudi I. o <imosudi@gmail.com>
2022-02-19 14:17:02 +0100

    Tool update -database column details

---
+++ mysqlconstantsFieldType.py
@@ -0,0 +1,32 @@
+#Read here for the list of column types:
+#http://mysql-python.sourceforge.net/MySQLdb-1.2.2/public/MySQLdb.constants.FIELD_TYPE-module.html
+
+
+field_type = {
+ 0: 'DECIMAL',
+ 1: 'TINY',
+ 2: 'SHORT',
+ 3: 'LONG',
+ 4: 'FLOAT',
+ 5: 'DOUBLE',
+ 6: 'NULL',
+ 7: 'TIMESTAMP',
+ 8: 'LONGLONG',
+ 9: 'INT24',
+ 10: 'DATE',
+ 11: 'TIME',
+ 12: 'DATETIME',
+ 13: 'YEAR',
+ 14: 'NEWDATE',
+ 15: 'VARCHAR',
+ 16: 'BIT',
+ 246: 'NEWDECIMAL',
+ 247: 'INTERVAL',
+ 248: 'SET',
+ 249: 'TINY_BLOB',
+ 250: 'MEDIUM_BLOB',
+ 251: 'LONG_BLOB',
+ 252: 'BLOB',
+ 253: 'VAR_STRING',
+ 254: 'STRING',
+ 255: 'GEOMETRY' }
